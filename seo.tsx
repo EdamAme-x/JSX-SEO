71482dbe79f076199bdedf5771f755746c924551
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2023-11-29 14:12:25 +0900

    Update seo.tsx

--- seo.tsx
+++ seo.tsx
@@ -15,26 +15,26 @@ export function JSXSEO(props: SEO) {
   return <>
     <meta charset="utf-8">
       <title>{props.title}</title>
-      <meta name="description" content={props.description}>
+      <meta name="description" content={props.description} />
       <base href="/">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="icon shortcut" href={props.icon}>
+      <link rel="icon shortcut" href={props.icon} />
       {/* OGP */}
       <meta property="og:type" content="website">
-      <meta property="og:title" content={props.title}>
-      <meta property="og:description" content={props.description}>
-      <meta property="og:site_name" content={props.title}>
-      <meta property="og:url" content={props.url}>
-      <meta property="og:image" content={props.url + icon}>
+      <meta property="og:title" content={props.title} />
+      <meta property="og:description" content={props.description} />
+      <meta property="og:site_name" content={props.title} />
+      <meta property="og:url" content={props.url} />
+      <meta property="og:image" content={props.url + icon} />
       <meta property="og:image:width" content="1280">
       <meta property="og:image:height" content="720">
       <meta property="og:locale" content="ja_JP">
       {/* OGP for Twitter */}
       <meta name="twitter:card" content="summary">
-      <meta name="twitter:title" content={props.title}>
-      <meta name="twitter:description" content={props.description}>
-      <meta name="twitter:image" content={props.url + icon}>
-      <meta name="twitter:creator" content={props.creater ?? "@amex2189"}>
+      <meta name="twitter:title" content={props.title} />
+      <meta name="twitter:description" content={props.description} />
+      <meta name="twitter:image" content={props.url + icon} />
+      <meta name="twitter:creator" content={props.creater ?? "@amex2189"} />
       {raw}
   </>
 }
