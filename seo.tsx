63b6cf8c0a3e56a55f4e3105e17ecc7f1d21a2b1
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2023-11-29 14:27:56 +0900

    Update seo.tsx

--- seo.tsx
+++ seo.tsx
@@ -8,16 +8,17 @@ interface SEO {
     type: string
   },
   creater?: string,
-  raw?: any
+  raw?: any,
+  local?: string
 }
 
 export function JSXSEO(props: SEO) {
   return <>
-    <meta charset="utf-8">
+      <meta charset="utf-8" />
       <title>{props.title}</title>
       <meta name="description" content={props.description} />
-      <base href="/">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <base href="/" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon shortcut" href={props.icon} />
       {/* OGP */}
       <meta property="og:type" content="website">
@@ -26,9 +27,9 @@ export function JSXSEO(props: SEO) {
       <meta property="og:site_name" content={props.title} />
       <meta property="og:url" content={props.url} />
       <meta property="og:image" content={props.url + icon} />
-      <meta property="og:image:width" content="1280">
-      <meta property="og:image:height" content="720">
-      <meta property="og:locale" content="ja_JP">
+      <meta property="og:image:width" content="1280" />
+      <meta property="og:image:height" content="720" />
+      <meta property="og:locale" content={props.local ?? "ja_JP"} />
       {/* OGP for Twitter */}
       <meta name="twitter:card" content="summary">
       <meta name="twitter:title" content={props.title} />
